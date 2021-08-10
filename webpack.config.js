dccc128b114694e7a19d99ba937ab8785c17ac10
duncdrum <d.paterson@me.com>
2021-08-10 16:32:22 +0200

    ignore(webpack): copy config just in case

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@
  */
 // @ts-check
 const config = require('./gen-webpack.config.js');
+// const webpack = require('webpack');
+// const yargs = require('yargs');
 
 /**
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
@@ -13,5 +15,10 @@ config.module.rules.push({
     test: /\.js$/,
     loader: require.resolve('@theia/application-manager/lib/expose-loader')
 }); */
+
+// if (yargs.option('mode').argv.mode === 'development') {
+//     console.log('Development mode. Including process variable...');
+//     config.plugins.push(new webpack.ProvidePlugin({ process: require.resolve('process/browser') }));
+//   }
 
 module.exports = config;
